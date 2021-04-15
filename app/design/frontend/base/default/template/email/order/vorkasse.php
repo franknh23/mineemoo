d90299bd079b01a1f651ca6f533eacf613f8e894
Ronak Chauhan <r.chauhan@mzentrale.de>
2021-04-15 13:34:52 +0200

    add vorkasse email message

--- app/design/frontend/base/default/template/email/order/vorkasse.php
+++
@@ -1,5 +0,0 @@
-<?php
-$order = $this->getData('order');
-if(is_object($order) && $order->getPayment()->getMethodInstance()->getCode() == "banktransfer") {
-    echo "Bank IBAN : DE000 0000 0000 0000"
-} 
