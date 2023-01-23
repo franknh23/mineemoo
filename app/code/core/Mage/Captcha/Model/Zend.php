6cd628e7cce18dd9cd38e8ff1c0898f98f319297
ronak.chauhan1 <ronak.chauhan@merkle.com>
2023-01-23 13:29:48 +0100

    MNM-101: upgarde to openmage

--- app/code/core/Mage/Captcha/Model/Zend.php
+++ app/code/core/Mage/Captcha/Model/Zend.php
@@ -1,27 +1,22 @@
 <?php
 /**
- * Magento
+ * OpenMage
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * https://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magento.com for more information.
- *
- * @category    Mage
- * @package     Mage_Captcha
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @category   Mage
+ * @package    Mage_Captcha
+ * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
+ * @copyright  Copyright (c) 2019-2022 The OpenMage Contributors (https://www.openmage.org)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
@@ -36,17 +31,17 @@ class Mage_Captcha_Model_Zend extends Zend_Captcha_Image implements Mage_Captcha
     /**
      * Key in session for captcha code
      */
-    const SESSION_WORD = 'word';
+    public const SESSION_WORD = 'word';
 
     /**
      * Min captcha lengths default value
      */
-    const DEFAULT_WORD_LENGTH_FROM = 3;
+    public const DEFAULT_WORD_LENGTH_FROM = 3;
 
     /**
      * Max captcha lengths default value
      */
-    const DEFAULT_WORD_LENGTH_TO   = 5;
+    public const DEFAULT_WORD_LENGTH_TO   = 5;
 
     /**
      * Helper Instance
@@ -71,7 +66,7 @@ class Mage_Captcha_Model_Zend extends Zend_Captcha_Image implements Mage_Captcha
      * Captcha form id
      * @var string
      */
-    protected  $_formId;
+    protected $_formId;
 
     /**
      * Zend captcha constructor
@@ -116,7 +111,7 @@ class Mage_Captcha_Model_Zend extends Zend_Captcha_Image implements Mage_Captcha
      */
     public function isRequired($login = null)
     {
-        $nonAuthForms = array('wishlist_sharing', 'sendfriend_send');
+        $nonAuthForms = ['wishlist_sharing', 'sendfriend_send'];
 
         if ((!in_array($this->_formId, $nonAuthForms) && $this->_isUserAuth())
             || !$this->_isEnabled() || !in_array($this->_formId, $this->_getTargetForms())
@@ -201,7 +196,7 @@ class Mage_Captcha_Model_Zend extends Zend_Captcha_Image implements Mage_Captcha
     /**
      * Whether to respect case while checking the answer
      *
-     * @return bool
+     * @return string
      */
     public function isCaseSensitive()
     {
@@ -266,7 +261,7 @@ class Mage_Captcha_Model_Zend extends Zend_Captcha_Image implements Mage_Captcha
         $storedWord = $this->getWord();
         $this->_clearWord();
 
-        if (!$word || !$storedWord){
+        if (!$word || !$storedWord) {
             return false;
         }
 
@@ -287,11 +282,11 @@ class Mage_Captcha_Model_Zend extends Zend_Captcha_Image implements Mage_Captcha
         return Mage::getSingleton('customer/session');
     }
 
-     /**
-     * Return full URL to captcha image
-     *
-     * @return string
-     */
+    /**
+    * Return full URL to captcha image
+    *
+    * @return string
+    */
     public function getImgSrc()
     {
         return $this->getImgUrl() . $this->getId() . $this->getSuffix();
@@ -301,7 +296,7 @@ class Mage_Captcha_Model_Zend extends Zend_Captcha_Image implements Mage_Captcha
      * log Attempt
      *
      * @param string $login
-     * @return Mage_Captcha_Model_Zend
+     * @return $this
      */
     public function logAttempt($login)
     {
@@ -430,7 +425,7 @@ class Mage_Captcha_Model_Zend extends Zend_Captcha_Image implements Mage_Captcha
     /**
      * Whether captcha is enabled at this area
      *
-     * @return bool
+     * @return string
      */
     protected function _isEnabled()
     {
@@ -469,8 +464,9 @@ class Mage_Captcha_Model_Zend extends Zend_Captcha_Image implements Mage_Captcha
      */
     protected function _setWord($word)
     {
-        $this->getSession()->setData($this->_getFormIdKey(self::SESSION_WORD),
-            array('data' => $word, 'expires' => time() + $this->getTimeout())
+        $this->getSession()->setData(
+            $this->_getFormIdKey(self::SESSION_WORD),
+            ['data' => $word, 'expires' => time() + $this->getTimeout()]
         );
         $this->_word = $word;
         return $this;
@@ -479,7 +475,7 @@ class Mage_Captcha_Model_Zend extends Zend_Captcha_Image implements Mage_Captcha
     /**
      * Set captcha word
      *
-     * @return Mage_Captcha_Model_Zend
+     * @return $this
      */
     protected function _clearWord()
     {
--- lib/Varien/Autoload.php
+++ lib/Varien/Autoload.php
@@ -1,27 +1,22 @@
 <?php
 /**
- * Magento
+ * OpenMage
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * https://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magento.com for more information.
- *
- * @category    Varien
- * @package     Varien_Autoload
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @category   Varien
+ * @package    Varien_Autoload
+ * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
+ * @copyright  Copyright (c) 2018 The OpenMage Contributors (https://www.openmage.org)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
@@ -29,36 +24,17 @@
  */
 class Varien_Autoload
 {
-    const SCOPE_FILE_PREFIX = '__';
-
-    static protected $_instance;
-    static protected $_scope = 'default';
-
-    protected $_isIncludePathDefined= null;
-    protected $_collectClasses      = false;
-    protected $_collectPath         = null;
-    protected $_arrLoadedClasses    = array();
-
     /**
-     * Class constructor
+     * @var Varien_Autoload
      */
-    public function __construct()
-    {
-        register_shutdown_function(array($this, 'destroy'));
-        $this->_isIncludePathDefined = defined('COMPILER_INCLUDE_PATH');
-        if (defined('COMPILER_COLLECT_PATH')) {
-            $this->_collectClasses  = true;
-            $this->_collectPath     = COMPILER_COLLECT_PATH;
-        }
-        self::registerScope(self::$_scope);
-    }
+    protected static $_instance;
 
     /**
      * Singleton pattern implementation
      *
      * @return Varien_Autoload
      */
-    static public function instance()
+    public static function instance()
     {
         if (!self::$_instance) {
             self::$_instance = new Varien_Autoload();
@@ -69,9 +45,9 @@ class Varien_Autoload
     /**
      * Register SPL autoload function
      */
-    static public function register()
+    public static function register()
     {
-        spl_autoload_register(array(self::instance(), 'autoload'));
+        spl_autoload_register([self::instance(), 'autoload']);
     }
 
     /**
@@ -81,91 +57,6 @@ class Varien_Autoload
      */
     public function autoload($class)
     {
-        if ($this->_collectClasses) {
-            $this->_arrLoadedClasses[self::$_scope][] = $class;
-        }
-        if ($this->_isIncludePathDefined) {
-            $classFile =  COMPILER_INCLUDE_PATH . DIRECTORY_SEPARATOR . $class;
-        } else {
-            $classFile = str_replace(' ', DIRECTORY_SEPARATOR, ucwords(str_replace('_', ' ', $class)));
-        }
-        $classFile.= '.php';
-        //echo $classFile;die();
-        return @include $classFile;
-    }
-
-    /**
-     * Register autoload scope
-     * This process allow include scope file which can contain classes
-     * definition which are used for this scope
-     *
-     * @param string $code scope code
-     */
-    static public function registerScope($code)
-    {
-        self::$_scope = $code;
-        if (defined('COMPILER_INCLUDE_PATH')) {
-            @include_once COMPILER_INCLUDE_PATH . DIRECTORY_SEPARATOR . self::SCOPE_FILE_PREFIX.$code.'.php';
-        }
-    }
-
-    /**
-     * Get current autoload scope
-     *
-     * @return string
-     */
-    static public function getScope()
-    {
-        return self::$_scope;
-    }
-
-    /**
-     * Class destructor
-     */
-    public function destroy()
-    {
-        if ($this->_collectClasses) {
-            $this->_saveCollectedStat();
-        }
-    }
-
-    /**
-     * Save information about used classes per scope with class popularity
-     * Class_Name:popularity
-     *
-     * @return Varien_Autoload
-     */
-    protected function _saveCollectedStat()
-    {
-        if (!is_dir($this->_collectPath)) {
-            @mkdir($this->_collectPath);
-            @chmod($this->_collectPath, 0777);
-        }
-
-        if (!is_writeable($this->_collectPath)) {
-            return $this;
-        }
-
-        foreach ($this->_arrLoadedClasses as $scope => $classes) {
-            $file = $this->_collectPath.DIRECTORY_SEPARATOR.$scope.'.csv';
-            $data = array();
-            if (file_exists($file)) {
-                $data = explode("\n", file_get_contents($file));
-                foreach ($data as $index => $class) {
-                    $class = explode(':', $class);
-                    $searchIndex = array_search($class[0], $classes);
-                    if ($searchIndex !== false) {
-                        $class[1]+=1;
-                        unset($classes[$searchIndex]);
-                    }
-                    $data[$index] = $class[0].':'.$class[1];
-                }
-            }
-            foreach ($classes as $class) {
-                $data[] = $class . ':1';
-            }
-            file_put_contents($file, implode("\n", $data));
-        }
-        return $this;
+        return @include str_replace(' ', DIRECTORY_SEPARATOR, ucwords(str_replace('_', ' ', $class))) . '.php';
     }
 }
